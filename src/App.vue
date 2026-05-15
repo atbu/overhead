e6f2be4f44146c3753c5d9965cbcb5186552fad7
Alex Burrows <186313861+atbu@users.noreply.github.com>
2026-05-15 21:50:53 +0100

    Parse NOTAM data and show it on the map

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,41 @@
 <script setup lang="ts">
-const response = await fetch(
-    'https://raw.githubusercontent.com/Jonty/uk-notam-archive/main/data/PIB.xml'
-);
-const text = await response.text();
-const parser = new DOMParser();
-const xml = parser.parseFromString(text, 'application/xml');
-console.log(xml);
+import "leaflet/dist/leaflet.css";
+import { LCircle, LMap, LTileLayer } from "@vue-leaflet/vue-leaflet";
+import { onMounted, type Ref, ref } from "vue";
+import { type ParsedNOTAM, parseNOTAMs } from "@/composables/useNOTAMParser.ts";
+
+const zoom = ref(6);
+const centre = ref([54.5, -2.5]);
+
+const parsedNotams: Ref<ParsedNOTAM[]> = ref([]);
+
+onMounted(async () => {
+    const response = await fetch(
+        "https://raw.githubusercontent.com/Jonty/uk-notam-archive/main/data/PIB.xml",
+    );
+    const text = await response.text();
+    const parser = new DOMParser();
+    const xml = parser.parseFromString(text, "application/xml");
+
+    parsedNotams.value = parseNOTAMs(xml);
+    console.log(`Parsed ${parsedNotams.value.length} NOTAMs.`);
+});
 </script>
 
 <template>
-    <h1>You did it!</h1>
-    <p>
-        Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-        documentation
-    </p>
+    <div style="height: 90vh; width: 100%">
+        <l-map ref="map" v-model:zoom="zoom" v-model:center="centre" :useGlobalLeaflet="false">
+            <l-tile-layer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+            <l-circle
+                v-for="notam in parsedNotams"
+                :key="notam.number"
+                :lat-lng="[notam.latitude, notam.longitude]"
+                :radius="notam.radiusInMetres"
+                :color="'#FF0000'"
+                :fill-opacity="0.2"
+            />
+        </l-map>
+    </div>
 </template>
 
 <style scoped></style>
---
+++ src/composables/useNOTAMParser.ts
@@ -0,0 +1,67 @@
+export interface ParsedNOTAM {
+    number: string,
+    latitude: number,
+    longitude: number,
+    radiusInMetres: number
+}
+
+function parseCoordinates(coordinates: string): {latitude: number, longitude: number} {
+    const latitudeString = coordinates.slice(0, 5);
+    const longitudeString = coordinates.slice(5, 11);
+
+    const latitudeDegrees = parseInt(latitudeString.slice(0, 2));
+    const latitudeMinutes = parseInt(latitudeString.slice(2, 4));
+    const isLatitudeNorth = latitudeString[4] == 'N';
+    const decimalisedLatitude = latitudeDegrees + (latitudeMinutes / 60);
+
+    const longitudeDegrees = parseInt(longitudeString.slice(0, 3));
+    const longitudeMinutes = parseInt(longitudeString.slice(3, 5));
+    const isLongitudeEast = longitudeString[5] == 'E';
+    const decimalisedLongitude = longitudeDegrees + (longitudeMinutes / 60);
+
+    return {
+        latitude: isLatitudeNorth ? decimalisedLatitude : -decimalisedLatitude,
+        longitude: isLongitudeEast ? decimalisedLongitude : -decimalisedLongitude
+    };
+}
+
+function convertRadiusToMetres(radiusNauticalMiles: number): number {
+    const METRES_PER_NM = 1852;
+    return radiusNauticalMiles * METRES_PER_NM;
+}
+
+export function parseNOTAMs(document: Document): ParsedNOTAM[] {
+    let output: ParsedNOTAM[] = [];
+
+    const notams = [
+        ...Array.from(document.getElementsByTagName("Notam"))
+    ];
+
+    notams.forEach((notam: Element) => {
+        const getText = (tag: string) =>
+            notam.getElementsByTagName(tag)[0]?.textContent?.trim() ?? '';
+
+        const coordinatesString = getText("Coordinates");
+        const radius: number = parseInt(getText("Radius"));
+
+        // For now, ignore NOTAMs that are too big or don't have a radius for visibility, we can sort this later
+        // TODO
+        if (radius == 0 || radius >= 20) {
+            return;
+        }
+
+        const {latitude, longitude} = parseCoordinates(coordinatesString);
+        const radiusInMetres = convertRadiusToMetres(radius);
+
+        const currentNOTAM: ParsedNOTAM = {
+            number: getText("Number"),
+            latitude,
+            longitude,
+            radiusInMetres
+        }
+
+        output.push(currentNOTAM);
+    });
+
+    return output;
+}
